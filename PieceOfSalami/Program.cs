702c4cf6454498b498d4a03400f50f57d2658a53
Mirectape <91116597+Mirectape@users.noreply.github.com>
2022-10-15 15:46:33 +0500

    Add project files.

---
+++ PieceOfSalami/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace PieceOfSalami
+{
+    struct S
+    {
+        public int a;
+        
+    }
+
+    class C
+    {
+        public int a = 0;
+    }
+
+        class Program
+    {
+
+        static void Main()
+        {
+            bool Super(ref int a)
+            {
+                a++;
+                return true;
+            }
+
+            int a = 1;
+            if(Super(ref a) && Super(ref a)) { }
+            Console.WriteLine(a);
+            S s = new S() { a = 1 };
+            C c = new C();
+            Func(c, s);
+            Console.WriteLine(c.a);
+            Console.WriteLine(s.a);
+        }
+        static void Func(C c, S s)
+        {
+            c.a++;
+            s.a++;
+        }
+    }
+
+}
+
+   
+
+
